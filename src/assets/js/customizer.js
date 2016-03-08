ccebcd59a14ca0f8e1e5bce25b583359704e4cab
Tim Hartwick <tdhartwick2@gmail.com>
2016-03-08 13:52:38 -0800

    cloned from tux

---
+++ src/assets/js/customizer.js
@@ -0,0 +1,13 @@
+$("#customizer-neckwear-thumb").click(function(){
+    $(".customizer-panel").addClass("is-active");
+});
+$("#close-customizer").click(function(){
+    $(".customizer-panel").removeClass("is-active");
+});
+
+$("#customizer-neckwear-thumb").click(function(){
+    $(".customizer-panel-overlay").addClass("is-active");
+});
+$("#close-customizer").click(function(){
+    $(".customizer-panel-overlay").removeClass("is-active");
+});
