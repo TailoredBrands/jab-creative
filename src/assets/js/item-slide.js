3a3e7c94dac09631a37430d9b3d10a264e0a5723
Tim Hartwick <tdhartwick2@gmail.com>
2016-03-09 11:13:31 -0800

    slick slider working

--- src/assets/js/item-slide.js
+++ src/assets/js/item-slide.js
@@ -1,7 +1,40 @@
 $(function() {
-    if (window.innerWidth <= 640) {
-        var carousel = $('.product-image-small ul').itemslide({
-            parent_width: true
-        });
-    }
+  if (window.innerWidth <= 640) {
+    var carousel = $('.product-image-small ul').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      useTransform: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1.25,
+            slidesToScroll: 1
+          }
+        }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+      ]
+    });
+  }
 })
