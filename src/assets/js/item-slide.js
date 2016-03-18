0cd154f4771820c699f47e4c283a375f97116c1b
Mandi Saeteun <mandi@zurb.com>
2016-03-18 14:41:21 -0700

    reset from the last commit

--- src/assets/js/item-slide.js
+++ src/assets/js/item-slide.js
@@ -35,77 +35,64 @@ $(function() {
           // settings: "unslick"
           // instead of a settings object
       ]
-    });
+    });   
+  }
 
-    var carousel = $('#productRecommendations').slick({
-      dots: false,
-      infinite: false,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      useTransform: true,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 3.25,
-            slidesToScroll: 1
-          }
+  var carousel = $('#productRecommendations').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    useTransform: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
         }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-      ]
-    });
-
-
-    var carousel = $('#recentlyViewed').slick({
-      dots: false,
-      infinite: false,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      useTransform: true,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 3.25,
-            slidesToScroll: 1
-          }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
         }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3.25,
+          slidesToScroll: 1
+        }
+      }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+  });
+
+
+  var carousel = $('#recentlyViewed').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    useTransform: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+<<<<<<< HEAD
           // You can unslick at a given breakpoint now by adding:
           // settings: "unslick"
           // instead of a settings object
@@ -151,3 +138,26 @@ $(function() {
     });
   }
 })
+=======
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3.25,
+          slidesToScroll: 1
+        }
+      }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+  });
+})
+>>>>>>> 7aeee3efc9abd607b6c6019f425df5db92424d23
