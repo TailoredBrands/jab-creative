1ff0d3c0e76a3f085fb44fd0da0cd4018e57aa23
Tim Hartwick <tdhartwick2@gmail.com>
2016-03-29 15:01:51 -0700

    cleaned the JS file

--- src/assets/js/item-slide.js
+++ src/assets/js/item-slide.js
@@ -1,11 +1,9 @@
-
-//Interchange and slick playing nicely 
+//Interchange and slick need to play nicely!
 $('.carouselInterchange').on('replace', function(){
 
-console.log("js here");
-
-$(function() {
+  //if small screen
   if (window.innerWidth <= 640) {
+    //pdp
     var carousel = $('.product-image-small ul').slick({
       dots: true,
       infinite: false,
@@ -37,14 +35,12 @@ $(function() {
             slidesToScroll: 1
           }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
       ]
-
     });   
   }
+  //end if
 
+  //PDP recommendations
   var carousel = $('#productRecommendations').slick({
     dots: false,
     infinite: false,
@@ -76,15 +72,11 @@ $(function() {
           slidesToScroll: 1
         }
       }
-
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-
     ]
   });
+  //End PDP recommendations
 
-
+  // Recently viewed
   var carousel = $('#recentlyViewed').slick({
     dots: false,
     infinite: false,
@@ -107,53 +99,49 @@ $(function() {
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
-})
+  //End recently viewed
 
-//Browse
-var carousel = $('.browsepage-thumbnails ul').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  useTransform: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+
+  //Browse
+  var carousel = $('.browsepage-thumbnails ul').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    useTransform: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+    ]
+  });
 });
+//End small screens logic
 
-});
-
+//PDP Recommendation
 var carousel = $('#productRecommendationsShopping').slick({
   dots: false,
   infinite: false,
@@ -185,12 +173,7 @@ var carousel = $('#productRecommendationsShopping').slick({
         slidesToScroll: 1
       }
     }
-
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-
   ]
 });
-
+//End PDP Recommendation
 
