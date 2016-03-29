7552ba821e1c23e10858668173a0201bfd4d3b42
Tim Hartwick <tdhartwick2@gmail.com>
2016-03-29 08:48:46 -0700

    updatd recommended items

--- src/assets/js/item-slide.js
+++ src/assets/js/item-slide.js
@@ -165,8 +165,8 @@ var carousel = $('#productRecommendationsShopping').slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 5,
+        slidesToScroll: 5,
         infinite: true,
         dots: true
       }
@@ -174,8 +174,8 @@ var carousel = $('#productRecommendationsShopping').slick({
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToShow: 4,
+        slidesToScroll: 4
       }
     },
     {
