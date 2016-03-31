28c13af8e16201e9d1d80e08aca06113e50ccd19
Tim Hartwick <tdhartwick2@gmail.com>
2016-03-30 20:13:21 -0700

    documented the panels to make it really easy to code

--- src/assets/js/shopping-modal.js
+++ src/assets/js/shopping-modal.js
@@ -1,9 +1,9 @@
 //Edit modal
 $('#shoppingEdit, #shoppingBagEditModalClose').click(function() {
-  $('.shopping-bag-edit-modal').toggleClass("is-active");
-  $('.shopping-bag-modal-title').toggleClass("is-active");
+  $('.bag-edit-modal').toggleClass("is-active");
+  $('.bag-edit-title').toggleClass("is-active");
   $('.edit-close').toggleClass("is-active");
-  $('.shopping-edit-update-cta').toggleClass("is-active");
+  $('.bag-edit-cta').toggleClass("is-active");
 
   //Fading
   $('body').toggleClass("is-scroll-locked");
@@ -11,12 +11,10 @@ $('#shoppingEdit, #shoppingBagEditModalClose').click(function() {
   $('.shopping-bag-recommendations').toggleClass("is-faded");
 });
 
-
-
 // //Sign in to Checkout
 $('#signIn, #sign-in-close').click(function() {
-  $('.shopping-bag-sign-in').toggleClass("is-active");
-  $('.shopping-bag-sign-in-cta').toggleClass("is-active");
+  $('.bag-sign-in').toggleClass("is-active");
+  $('.bag-sign-in-cta').toggleClass("is-active");
 
   //Fading
   $('body').toggleClass("is-scroll-locked");
