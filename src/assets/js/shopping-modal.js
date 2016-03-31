0845a2ce77de9416a536551463abbccb1c6a3f79
Tim Hartwick <tdhartwick2@gmail.com>
2016-03-30 22:04:37 -0700

    completed first pass at shopping bag for mobile

--- src/assets/js/shopping-modal.js
+++ src/assets/js/shopping-modal.js
@@ -21,3 +21,20 @@ $('#signIn, #sign-in-close').click(function() {
   $('.shopping-bag').toggleClass("is-faded");
   $('.shopping-bag-recommendations').toggleClass("is-faded");
 });
+
+// Forgot Panel
+$('#bagForgotPassword, #forgotPasswordClose').click(function() {
+  //toggles the sign in panel
+  $('.bag-sign-in').toggleClass("is-active");
+  $('.bag-sign-in-cta').toggleClass("is-active");
+
+  //toggles the forgot panel
+  $('.bag-forgot').toggleClass("is-active");
+  $('.bag-forgot-cta').toggleClass("is-active");
+
+  //Fading
+  $('body').toggleClass("is-scroll-locked");
+  $('.shopping-bag').toggleClass("is-faded");
+  $('.shopping-bag-recommendations').toggleClass("is-faded");
+});
+
