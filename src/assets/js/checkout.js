e262c896729f28ef8763244bcf8bf457b22259c7
Tim Hartwick <tdhartwick2@gmail.com>
2016-04-01 15:46:43 -0700

    updated sticky and checkout show/hide change address

--- src/assets/js/checkout.js
+++ src/assets/js/checkout.js
@@ -2,3 +2,12 @@ $('#differentStore, #reserveInStore').click(function(){
   $('.store-saved').toggleClass('is-active');
   $('.store-new').toggleClass('is-active');
 });
+
+//Change address
+
+$('#changeAddress, #changeAddress2, #saveAddress').click(function(){
+  $('#dropdown-ship-address-1').toggleClass('is-active');
+  $('#ChangeAddressInputs').toggleClass('is-active');
+  $('#changeAddress').toggleClass('is-active');
+  $('#saveAddress').toggleClass('is-active');
+});
