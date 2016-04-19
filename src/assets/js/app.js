38dde46d0d6a0d073838c295d9a73cff692b5ebf
Tim Hartwick <tdhartwick2@gmail.com>
2016-04-18 20:58:33 -0700

    menu works

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -127,9 +127,23 @@ $('#pdpSeeMore').on('click', function() {
 //Off-canvas
 $('#menuIcon').on('click', function(){
   $('.inner-wrap').toggleClass('is-active');
+  $('.off-canvas-wrap').toggleClass('is-active');
 });
 
 $('#suitMenu').on('click', function(){
   $('#suitSubMenu').toggleClass('is-active');
 });
 
+
+$(function() {   
+ $(window).resize(function() {
+        //small-screen
+        if (window.innerWidth >= 640) {
+          $('#suitMenu, #suitSubMenu').hover( function(){
+            $('#suitSubMenu').toggleClass('is-active');
+          });
+        }
+        //end small-screen
+    }) .resize(); // trigger resize event
+
+})
