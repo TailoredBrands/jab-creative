cf438855ae66c447ebe3500b2c07f34f3552518b
Tim Hartwick <tdhartwick2@gmail.com>
2016-04-21 08:10:00 -0700

    filter page for desktop

--- src/assets/js/slick-slider.js
+++ src/assets/js/slick-slider.js
@@ -83,41 +83,19 @@ $('.carouselInterchange').on('replace', function(){
 
 
   //Browse
-  var carousel = $('.browsepage-thumbnails ul').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    useTransform: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
 });
 //End small screens logic
+
+var carousel = $('.browsepage-thumbnails ul').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  useTransform: true
+});
+
 
 //Shopping Bag Recommendation
 var carousel = $('#productRecommendationsShopping').slick({
