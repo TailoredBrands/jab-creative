bc195b0ffccace036596d8996f94f224ce514a3c
Tim Hartwick <tdhartwick2@gmail.com>
2016-04-27 16:08:56 -0700

    fixed that pesky slider error

--- src/assets/js/slick-slider.js
+++ src/assets/js/slick-slider.js
@@ -81,6 +81,39 @@ $('.carouselInterchange').on('replace', function(){
   });
   //End PDP recommendations
 
+  var carousel = $('#productRecommendationsTwo').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    useTransform: true,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        infinite: true,
+      }
+    },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 4.25,
+          slidesToScroll: 4
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3.25,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
 
   //Browse
 });
@@ -204,37 +237,3 @@ var carousel = $('#quickViewRecommendations').slick({
 });
 //End QuickView Recommendation
 
-// Recently viewed
-var carousel = $('#recentlyViewed').slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  useTransform: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 6,
-        slidesToScroll: 6,
-        infinite: true,
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 4.25,
-        slidesToScroll: 4
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 3.25,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
-//End recently viewed
