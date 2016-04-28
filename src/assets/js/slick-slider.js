41a5ff7277d7e1fb3a51673aa3fc50090a92e33d
James Stone <james@jamesstone.co>
2016-04-27 17:29:02 -0700

    slick adjustments after merge

--- src/assets/js/slick-slider.js
+++ src/assets/js/slick-slider.js
@@ -85,8 +85,8 @@ $('.carouselInterchange').on('replace', function(){
     dots: false,
     infinite: false,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 6,
+    slidesToScroll: 6,
     useTransform: true,
     responsive: [
     {
